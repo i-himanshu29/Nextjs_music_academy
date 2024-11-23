e79ea66ea71fada110733b0ee251393af4aa87c7
Himanshu Maurya <mauryahimanshu979@gmail.com>
2024-11-23 13:13:13 +0530

    first commit music academy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**', // Matches all paths
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**', // Matches all paths
+      },
+    ],
+  },
 };
 
 export default nextConfig;
